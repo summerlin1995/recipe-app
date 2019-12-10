458bbe4cad4ea269867cb0f4498d0a862a2ca6d3
summerlin <summer.lin@logicsolutions.com>
2019-12-10 14:10:49 +0800

    manually create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule
